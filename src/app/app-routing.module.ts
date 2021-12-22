e5937beb34eeba63cc7eeb41ffda149a7ad170f1
Cyril DEFAYE <cyril.defaye@monext.net>
2021-12-23 00:40:16 +0100

    version 0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CarListComponent} from "./car/list/car-list.component";
+import {MaintenanceListComponent} from "./maintenance/list/maintenance-list.component";
+import {HomeComponent} from "./core/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cars', component: CarListComponent },
+  { path: 'maintenances', component: MaintenanceListComponent },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
